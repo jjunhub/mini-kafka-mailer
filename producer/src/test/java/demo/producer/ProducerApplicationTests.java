c768654466da1d1abd51121ea0e19c56b3f5424f
sangjun <sangjun1389@ajou.ac.kr>
2025-05-17 00:38:28 +0900

    producer 모듈 추가

--- producer/src/test/java/demo/producer/ProducerApplicationTests.java
+++ producer/src/test/java/demo/producer/ProducerApplicationTests.java
@@ -1,10 +1,10 @@
-package demo.com;
+package demo.producer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ComApplicationTests {
+class ProducerApplicationTests {
 
     @Test
     void contextLoads() {
